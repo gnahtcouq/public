7f34344606692e1d55689574669e80a4e6df468b
Quốc Thắng <comehere.thang@gmail.com>
2022-11-14 11:56:05 +0700

    Add files via upload

---
+++ CKhoiA.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bai6 {
+    public class CKhoiA:CThiSinh {
+        protected double m_DiemToan;
+        protected double m_DiemLy;
+        protected double m_DiemHoa;
+        public double DiemToan {
+            get { return m_DiemToan; }
+            set { m_DiemToan = value; }
+        }
+        public double DiemLy {
+            get { return m_DiemLy; }
+            set { m_DiemLy = value; }
+        }
+        public double DiemHoa {
+            get { return m_DiemHoa; }
+            set { m_DiemHoa = value; }
+        }
+        public CKhoiA() : base() {
+            m_DiemToan = 0;
+            m_DiemLy = 0;
+            m_DiemHoa = 0;
+        }
+        public CKhoiA(string ma, string ht, double toan, double ly, double hoa) : base (ma, ht) {
+            m_DiemToan = toan;
+            m_DiemLy = ly;
+            m_DiemHoa = hoa;
+        }
+
+        public override double TongDiem() {
+            return m_DiemToan + m_DiemLy + m_DiemHoa;
+        }
+        public override KieuKetQua KetQua() {
+            if (TongDiem() >= 18.5)
+                return KieuKetQua.Dat;
+            else
+                return KieuKetQua.KhongDat;
+        }
+        public override bool laKhoiA() {
+            return true;
+        }
+        public override bool laKhoiNangKhieu() {
+            return false;
+        }
+    }
+}
---
+++ CThiSinh.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bai6 {
+    public enum KieuKetQua { Dat, KhongDat }
+    public class CThiSinh {
+        protected string m_MaTS;
+        protected string m_Hoten;
+        public string MaTS {
+            get { return m_MaTS; }
+            set { m_MaTS = value; }
+        }
+        public string Hoten {
+            get { return m_Hoten; }
+            set { m_Hoten = value; }
+        }
+        public CThiSinh() {
+            m_MaTS = "";
+            m_Hoten = "";
+        }
+        public CThiSinh(string ma, string ht) {
+            m_MaTS = ma;
+            m_Hoten = ht;
+        }
+        public virtual double TongDiem() {
+            return 0;
+        }
+        public virtual KieuKetQua KetQua() {
+            return KieuKetQua.KhongDat;
+        }
+
+        public virtual bool laKhoiA() {
+            return false;
+        }
+
+        public virtual bool laKhoiNangKhieu() {
+            return false;
+        }
+    }
+}
