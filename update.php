6af899a4a7ab6907dff9fae81500a0b6f88efa3e
Quốc Thắng <comehere.thang@gmail.com>
2023-11-22 11:31:16 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,44 +1,12 @@
 <?php
 include("connect.php");
 
-$id = $_GET['id'] ?? '';
+$pub_id = $_POST['id'] ?? '';
+$pub_name = $_POST['name'] ?? '';
 
-if ($id != '') {
-    // Tìm nạp dữ liệu hiện có cho ID nhà xuất bản được chỉ định
-    $sql = 'select * from publisher where pub_id = ?';
+if ($pub_id != '') {
+    $sql = 'update publisher set pub_name = ? where pub_id = ?';
     $stm = $conn->prepare($sql);
-    $stm->execute([$id]);
-    $existingData = $stm->fetch(PDO::FETCH_ASSOC);
-
-    if ($existingData) {
-        // Hiển thị form để chỉnh sửa
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // Xử lý việc gửi biểu mẫu để cập nhật bản ghi
-            $name = $_POST['name'] ?? '';
-
-            if (!empty($name)) {
-                // Cập nhật bản ghi vào cơ sở dữ liệu
-                $sql = 'update publisher set pub_name = ? where pub_id = ?';
-                $stm = $conn->prepare($sql);
-                $stm->execute([$name, $id]);
-
-                header('location:index.php');
-                exit();
-            } else {
-                echo "Vui lòng cung cấp tên nhà xuất bản hợp lệ.";
-            }
-        }
-?>
-        <form action="update.php?id=<?php echo $existingData['pub_id']; ?>" method="post">
-            <input type="hidden" name="id" value="<?php echo $existingData['pub_id']; ?>">
-            Mã nhà xuất bản <input type="text" name="name" value="<?php echo $existingData['pub_name']; ?>"> <br>
-            <input type="submit" value="Cập nhật">
-        </form>
-<?php
-    } else {
-        echo "Không tìm thấy bản ghi.";
-    }
-} else {
-    echo "ID nhà xuất bản không hợp lệ.";
+    $stm->execute([$pub_name, $pub_id]);
 }
-?>
+header('location:index.php');
